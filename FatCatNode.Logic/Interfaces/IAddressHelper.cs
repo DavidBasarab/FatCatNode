e94eaf4c28337cf0ece8062445cbda38b8fc26f3
David.Basarab <dbasarab617@gmail.com>
2011-02-20 00:39:28 -0500

    Created IAddressHelper

---
+++ FatCatNode.Logic/Interfaces/IAddressHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace FatCatNode.Logic.Interfaces
+{
+    public interface IAddressHelper
+    {
+        void SetNodeId(string nodeId);
+
+        IPAddress FindComputerIpAddress();
+
+        Uri FindBaseAddress();
+    }
+}
