7dcbe46b59f273d6e0ba5bbdc9d26f8ab6568fd9
David.Basarab.Jr <dbasarab617@gmail.com>
2011-02-21 00:04:27 -0500

    Added test OnCreationNodeWillInformTheAddressHelperOfTheNodeId.

---
+++ FatCatNode.Logic/AddressHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using FatCatNode.Logic.Interfaces;
+
+namespace FatCatNode.Logic
+{
+    public class AddressHelper : IAddressHelper
+    {
+        private static IAddressHelper _overridenHelper;
+
+        public static IAddressHelper Helper
+        {
+            get { return _overridenHelper ?? Nested.Instance; }
+            set { _overridenHelper = value; }
+        }
+
+        public void SetNodeId(string nodeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPAddress FindComputerIpAddress()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Uri FindBaseAddress()
+        {
+            throw new NotImplementedException();
+        }
+
+        private class Nested
+        {
+            internal static readonly AddressHelper Instance = new AddressHelper();
+        }
+    }
+}
---
+++ FatCatNode.Tests/NodeTest.cs
@@ -0,0 +1,29 @@
+﻿using FatCatNode.Logic;
+using FatCatNode.Logic.Interfaces;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace FatCatNode.Tests
+{
+    [TestFixture]
+    public class NodeTest
+    {
+        [Test]
+        public void OnCreationNodeWillInformTheAddressHelperOfTheNodeId()
+        {
+            var mocks = new MockRepository();
+
+            var addressHelper = mocks.DynamicMock<IAddressHelper>();
+
+            addressHelper.Expect(v => v.SetNodeId("UnitTestNode"));
+
+            mocks.ReplayAll();
+
+            AddressHelper.Helper = addressHelper;
+
+            var node = new Node("UnitTestNode");
+
+            mocks.VerifyAll();
+        }
+    }
+}
